8c4922c57e13e27cd30560ed1dd073767e63d00d
OanaAghebanoae <oana.aghebanoae@gmail.com>
2024-04-29 17:20:47 +0300

    Added component for hero details

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroesComponent } from './heroes/heroes.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
