a0f5d4cf544d175e813888b51f769fe732e177d7
OanaAghebanoae <oana.aghebanoae@gmail.com>
2024-04-30 00:02:15 +0300

    Added search bar, mocked server response

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {NgIf, UpperCasePipe} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {Hero} from '../hero';
+import { Component, Input } from '@angular/core';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from '../hero.service';
@@ -20,12 +20,12 @@ export class HeroDetailComponent {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
   }
-  
+
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
@@ -34,5 +34,12 @@ export class HeroDetailComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
